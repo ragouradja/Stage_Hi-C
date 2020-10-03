9137db794b84bc4676e349e166b8c8889917cfc8
ragouradja <71189947+ragouradja@users.noreply.github.com>
2020-10-03 17:24:46 +0200

    Delete data.py

--- data.py
+++
@@ -1,8 +0,0 @@
-with open("BindingSiteSet.txt","r") as f_in, open("position.dat","w") as f_out:
-	for line in f_in:
-		if not line.startswith("#"):
-			item = line.split("\t",maxsplit = 6)
-			if "NA" not in item:
-				f_out.write("{}\t{}\t{}\n".format("chr1",item[4],item[5]))
-
-
