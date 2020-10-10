9fc80b8957dd009f2840211b266eea5f360401f2
ragouradja <71189947+ragouradja@users.noreply.github.com>
2020-10-10 11:54:07 +0200

    Add files via upload

---
+++ chromosight_tf.py
@@ -0,0 +1,15 @@
+
+import os
+
+name_map = "contacts2_Coli_PM35_2000_df.bg2.cool"
+path_tf = "/home/ragou/Bureau/Stage_Hi-C/Pairs"
+
+file_tf = os.listdir(path_tf)
+
+for tf in file_tf:
+
+	name_tf = tf.split("_")[2].split(".")[0]
+	os.system("chromosight quantify ./../Pairs/{} {} ./results/{}".format(tf,name_map,name_tf))
+	
+		
+		
